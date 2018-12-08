9ae5aa5842e7a451e6246f05253466f8ca6d756a
Juan Manuel Mougan <juanmougan@gmail.com>
2018-12-08 18:13:13 -0300

    Hydrate storage on load

--- src/App.js
+++ src/App.js
@@ -55,6 +55,30 @@ class App extends Component {
     localStorage.setItem("list", JSON.stringify(updatedList));
   }
 
+  hydrateStateWithLocalStorage() {
+    // for all items in state
+    for (let key in this.state) {
+      // if the key exists in localStorage
+      if (localStorage.hasOwnProperty(key)) {
+        // get the key's value from localStorage
+        let value = localStorage.getItem(key);
+
+        // parse the localStorage string and setState
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          // handle empty string
+          this.setState({ [key]: value });
+        }
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.hydrateStateWithLocalStorage();
+  }
+
   render() {
     return (
       <div className="App">
